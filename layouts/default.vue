58a5e88714a04918e7102bec075cdb530233c352
Rostislav <rvssv@bk.ru>
2022-09-16 13:41:51 +0300

    added new branch feature_main_page

---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+  <main>
+    <div class="container">
+      <Nuxt />
+    </div>
+  </main>
+</template>
+
+<script>
+  export default {
+    name: 'defaultLayouts'
+  }
+</script>
+
+<style scoped>
+
+</style>
